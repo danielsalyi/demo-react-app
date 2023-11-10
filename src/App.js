7de673f2a9d51a61e55d8fb61a914c8217ef65fc
Daniel Salyi <d.salyi@student.tue.nl>
2023-11-10 23:25:10 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [name, setName] = useState('Dave')
+    const [count, setCount] = useState(0)
+
+    const handleNameChange = () => {
+        const names = ["bob", 'kevin', 'dave']
+        const int = Math.floor(Math.random() * 3)
+        setName(names[int])
+    }
+
+    const handleClick = () => {
+        setCount(count + 1)
+        console.log("asdasd")
+    }
+
+    return (
+        <main>
+            <p>
+                Hello {name} {count}!
+            </p>
+            <button onClick={handleNameChange}>Change name</button>
+            <button onClick={handleClick}>Change number</button>
+        </main>
+    );
 }
 
 export default App;
